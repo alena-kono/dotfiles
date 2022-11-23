1bede16b22e25e5c350a287223243a9770785c16
Alena Konovalova <konovalova.alena@gmail.com>
2022-11-23 15:38:10 +0300

    Update fish and kitty configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,8 @@ end
 
 export PATH="/usr/local/opt/python@3.10/bin:$PATH"
 export GOPATH="/Users/alena/go"
+
+alias psql="/usr/local/Cellar/postgresql@15/15.1/bin/psql"
+
+alias dark="kitty +kitten themes --reload-in=all Nord"
+alias light="kitty +kitten themes --reload-in=all Atom One Light"
