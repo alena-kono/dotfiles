90cb10fe88fb4b88e6def59347302e3f22058d96
Alena Konovalova <alena.kono@fastmail.com>
2024-02-16 15:40:04 +0300

    feat: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-export PATH="/usr/local/opt/python@3.10/bin:$PATH"
-export GOPATH="/Users/alena/go"
+export PATH="/opt/homebrew/bin/python@3.12/bin:$PATH"
 
-alias psql="/usr/local/Cellar/postgresql@15/15.1/bin/psql"
+# alias dark="kitty +kitten themes --cache-age=-1 --reload-in=all Catppuccin-Macchiato"
+# alias light="kitty +kitten themes --cache-age=-1 --reload-in=all Catppuccin-Latte"
+# alias dark="kitty +kitten themes --cache-age=-1 --reload-in=all Rosé Pine"
+# alias light="kitty +kitten themes --cache-age=-1 --reload-in=all Rosé Pine Dawn"
 
-alias dark="kitty +kitten themes --reload-in=all Nord"
-alias light="kitty +kitten themes --reload-in=all Atom One Light"
+alias dark="kitty +kitten themes --cache-age=-1 --reload-in=all Alabaster Dark"
+alias light="kitty +kitten themes --cache-age=-1 --reload-in=all Alabaster"
+
+set -x GOPATH $HOME/go
+set PATH $GOPATH/bin $GOROOT/bin $PATH
+set PATH $HOME/.cargo/bin $PATH
