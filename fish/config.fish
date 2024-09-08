87f9e30bb93859a5805aa671b0658ee77b22a47d
Alena Konovalova <alena.kono@fastmail.com>
2024-09-08 16:21:14 +0300

    feat(fish): update conf

--- fish/config.fish
+++ fish/config.fish
@@ -4,13 +4,8 @@ end
 
 export PATH="/opt/homebrew/bin/python@3.12/bin:$PATH"
 
-# alias dark="kitty +kitten themes --cache-age=-1 --reload-in=all Catppuccin-Macchiato"
-# alias light="kitty +kitten themes --cache-age=-1 --reload-in=all Catppuccin-Latte"
-# alias dark="kitty +kitten themes --cache-age=-1 --reload-in=all Rosé Pine"
-# alias light="kitty +kitten themes --cache-age=-1 --reload-in=all Rosé Pine Dawn"
-
-alias dark="kitty +kitten themes --cache-age=-1 --reload-in=all Alabaster Dark"
-alias light="kitty +kitten themes --cache-age=-1 --reload-in=all Alabaster"
+alias dark="kitty +kitten themes --cache-age=-1 Alabaster Dark"
+alias light="kitty +kitten themes --cache-age=-1 Alabaster"
 
 set -x GOPATH $HOME/go
 set PATH $GOPATH/bin $GOROOT/bin $PATH
